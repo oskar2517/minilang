7ec1f9337afcbeed9a9caa3ab285be4429705cd8
Senk Ju <12718920+oskar2517@users.noreply.github.com>
2023-07-04 22:16:39 +0200

    dont show context after executing

--- Main.hs
+++ Main.hs
@@ -1,11 +1,13 @@
 module Main where
 import Parser (parseAst)
 import Interpreter (execute, Context)
+import Control.Monad (void)
 
-main :: IO Context
+main :: IO ()
 main = do
     code <- readFile "program.txt"
     let ast = parseAst code
     case ast of
         Nothing -> error "Syntax error in file"
-        Just a -> execute a
+        Just a -> do 
+            void $ execute a
